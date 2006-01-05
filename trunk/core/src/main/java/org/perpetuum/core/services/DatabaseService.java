c7497215d6c56f9f54df4a1513cb7742e23fe55a
jcscoobyrs <jcscoobyrs@e9cb1332-0b05-0410-95d3-ec1d154528da>
2006-01-05 22:56:30 +0000

    Database design began and DDL should follow.

--- trunk/core/src/main/java/org/perpetuum/core/services/DatabaseService.java
+++ trunk/core/src/main/java/org/perpetuum/core/services/DatabaseService.java
@@ -1,9 +1,15 @@
 package org.perpetuum.core.services;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.ResourceBundle;
@@ -16,6 +22,12 @@ public class DatabaseService extends AbstractService {
 	private final String DATABASE_HOME = System.getProperty("perpetuum.home") + File.separator + "data";
 	private EmbeddedDataSource dataSource = null;
 	private Connection conn = null;
+	
+	/**
+     * SQLSTATE String that according to Derby documentation means
+     * that the table queried does not exist.
+     */
+    private static final String TABLE_DOES_NOT_EXIST = "42X05";
 	
 	public DatabaseService() {
 		prepare(NAME);
@@ -38,8 +50,61 @@ public class DatabaseService extends AbstractService {
 	}
 	
 	public void executeDDLIfNecessary() {
-		// We need to specify what needs to be prebuilt/prepopulated
-		// in the database and do that here.
+		Statement stmt = null;
+		String[] tables = new String[] { "USERS", "REALMS", "ROLES", "REALM_USERS" };
+		
+		for (int i = 0; i < tables.length; i++) {
+			try {
+				stmt = conn.createStatement();
+				
+				stmt.executeQuery("SELECT COUNT(1) FROM " + tables[i]);
+			} catch (SQLException sqle) {
+				String state = sqle.getSQLState();
+				
+				if (state.equals(TABLE_DOES_NOT_EXIST)) {
+					StringBuffer ddl = new StringBuffer();
+					InputStream in = DatabaseService.class.getClassLoader().getResourceAsStream("ddl/" + tables[i].toLowerCase() + ".ddl");
+					BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+					String cLine;
+					
+					try {
+						while ((cLine = bin.readLine()) != null) {
+							ddl.append(cLine);
+						}
+					} catch (IOException ioe) {
+						log.error(ioe.getMessage());
+						ioe.printStackTrace();
+					} finally {
+						try {
+							in.close();
+						} catch (IOException ioe) {
+							log.error(ioe.getMessage());
+							ioe.printStackTrace();
+						}
+					}
+					
+					if (ddl.length() > 0) {
+						try {
+							stmt = conn.createStatement();
+							
+							stmt.executeUpdate(ddl.toString());
+							
+							stmt.close();
+						} catch (SQLException sqle2) {
+							log.error(sqle2);
+							sqle2.printStackTrace();
+						}
+					}
+				}
+			} finally {
+				try {
+					stmt.close();
+				} catch (SQLException sqle) {
+					log.error(sqle.getMessage());
+					sqle.printStackTrace();
+				}
+			}
+		}
 	}
 	
 	public void setDatabaseProperties() throws Exception {
