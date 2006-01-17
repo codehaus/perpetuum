26a632a953250ea7c36a52f65bebafc84d2c9bb0
jcscoobyrs <jcscoobyrs@e9cb1332-0b05-0410-95d3-ec1d154528da>
2006-01-17 03:01:35 +0000

    Modified User object to have email instead of description and removed the start method from PerpetuumCoreMBean as you will never start the server from JMX.

--- core/src/main/java/org/perpetuum/core/management/PerpetuumCoreMBean.java
+++ core/src/main/java/org/perpetuum/core/management/PerpetuumCoreMBean.java
@@ -3,11 +3,6 @@ package org.perpetuum.core.management;
 public interface PerpetuumCoreMBean extends ServiceMBean {
 
 	/**
-	 * Starts the core and all of its dependencies
-	 */
-	public void start();
-	
-	/**
 	 * Stops the core and all of its dependencies
 	 */
 	public void stop();
