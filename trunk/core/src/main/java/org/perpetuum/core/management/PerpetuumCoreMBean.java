f35c29e8c2b70074d8ca945582f28d0270e28968
jcscoobyrs <jcscoobyrs@e9cb1332-0b05-0410-95d3-ec1d154528da>
2006-01-30 22:56:16 +0000

    Reconfiguring directory structure.

--- trunk/core/src/main/java/org/perpetuum/core/management/PerpetuumCoreMBean.java
+++
@@ -1,9 +0,0 @@
-package org.perpetuum.core.management;
-
-public interface PerpetuumCoreMBean extends ServiceMBean {
-
-	/**
-	 * Stops the core and all of its dependencies
-	 */
-	public void stop();
-}
